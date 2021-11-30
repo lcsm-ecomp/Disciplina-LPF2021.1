d976afc6b80f18f10fe7213347963c20c6feeb9b
lcsm-ecomp <lcsm@ecomp.poli.br>
2021-11-30 18:45:21 +0000

    Servidor

--- servidor.main.kts
+++ servidor.main.kts
@@ -1,18 +1,25 @@
 @file:Repository("https://jcenter.bintray.com")
-@file:DependsOn("org.springframework.boot:spring-boot-starter-web:2.5.7")
-@file:DependsOn("org.springframework.boot:spring-boot-starter-test:2.5.7")
+@file:DependsOn("org.springframework.boot:spring-boot-starter-web:2.6.0")
+@file:DependsOn("org.springframework.boot:spring-boot-starter-tomcat:2.6.0")
+
+package MeuServidor
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-
+import org.springframework.web.bind.annotation.*
 
 println("ola para todos")
 println("Teste de Modificacao no github")
 
 
 @SpringBootApplication
-class DemoApplication {
+//@RestController
+open class DemoApplication {
 
 }
 
-runApplication<DemoApplication>(*args)
+val arg = arrayOf(
+    "--server.port=3003",
+)
+
+runApplication<DemoApplication>(*arg)
