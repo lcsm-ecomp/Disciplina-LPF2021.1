f7047706e3df1bd6e291acc68438d2e7b30861ef
lcsm-ecomp <lcsm@ecomp.poli.br>
2021-11-30 18:14:23 +0000

    Teste

--- servidor.main.kts
+++ servidor.main.kts
@@ -1,3 +1,18 @@
+@file:Repository("https://jcenter.bintray.com")
+@file:DependsOn("org.springframework.boot:spring-boot-starter-web:2.5.7")
+@file:DependsOn("org.springframework.boot:spring-boot-starter-test:2.5.7")
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
 
 println("ola para todos")
 println("Teste de Modificacao no github")
+
+
+@SpringBootApplication
+class DemoApplication {
+
+}
+
+runApplication<DemoApplication>(*args)
