440295e87ef6ae372b691fc17eb5b52a07ee13a2
lcsm-ecomp <lcsm@ecomp.poli.br>
2021-12-02 15:07:20 +0000

    Versao da aula 2

--- FrontEnd.kt
+++ FrontEnd.kt
@@ -1,5 +1,30 @@
 //Programa que roda no navegador
+import kotlinx.browser.*
+
+
+@JsName("botaoLimpar")
+fun limpar() {
+    val elem = document.getElementById("p1")
+    if (elem!=null) {
+        elem.innerHTML = ""
+    }
+}
+
+@JsName("botaoClicado")
+fun botaoClicado(nome:String) {
+    println("O usuario clicou o botão $nome")
+    val elem = document.getElementById("p1")
+    if (elem!=null) {
+        elem.innerHTML += """
+            <br> <font color="$nome">Este texto foi inserido pelo programa Kotlin 
+            ao pressionar o botao $nome</font>
+            <button onclick="FrontEnd.botaoClicado('yellow')">Nao aperte esse botao</button>
+            """
+    }
+}
 
 fun main() {
     println("ola do navegador")
+    for (c in 1..100)
+      println("Linha impressa em kotlin numero $c")
 }
--- static/FrontEnd.js
+++ static/FrontEnd.js
@@ -3,9 +3,24 @@ if (typeof kotlin === 'undefined') {
 }var FrontEnd = function (_, Kotlin) {
   'use strict';
   var println = Kotlin.kotlin.io.println_s8jyv4$;
+  function limpar() {
+    var elem = document.getElementById('p1');
+    if (elem != null) {
+      elem.innerHTML = '';
+    }}
+  function botaoClicado(nome) {
+    println('O usuario clicou o bot\xE3o ' + nome);
+    var elem = document.getElementById('p1');
+    if (elem != null) {
+      elem.innerHTML = elem.innerHTML + ('\n' + '            <br> <font color=' + '"' + nome + '"' + '>Este texto foi inserido pelo programa Kotlin ' + '\n' + '            ao pressionar o botao ' + nome + '<\/font>' + '\n' + '            <button onclick=' + '"' + "FrontEnd.botaoClicado('yellow')" + '"' + '>Nao aperte esse botao<\/button>' + '\n' + '            ');
+    }}
   function main() {
     println('ola do navegador');
+    for (var c = 1; c <= 100; c++)
+      println('Linha impressa em kotlin numero ' + c);
   }
+  _.botaoLimpar = limpar;
+  _.botaoClicado = botaoClicado;
   _.main = main;
   main();
   Kotlin.defineModule('FrontEnd', _);
